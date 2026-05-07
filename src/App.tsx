20def88a29f11893476be45b78319845aacb6de3
CKinni-07 <chinni.krishna@fuzebox.ai>
2026-05-07 14:56:08 -0400

    feat: initial React + Vite boilerplate

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <main className="container">
+      <h1>Connect Sphere</h1>
+      <p>To foster meaningful connections and facilitate seamless communication among individuals.</p>
+    </main>
+  )
+}
+
+export default App
